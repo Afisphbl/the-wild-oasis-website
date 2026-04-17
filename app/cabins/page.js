6b4994920166fe4d3e31fac40b24599466c82b5d
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-04-17 07:52:57 +0300

    Implement user fetching in cabins page, update Logo and Navigation components to use Link, and add loading component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <h1>Cabins</h1>;
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  if (!res.ok) {
+    throw new Error("Failed to fetch users");
+  }
+  const usersData = await res.json();
+
+  return (
+    <div>
+      <h1>Cabins</h1>
+      <ul>
+        {usersData.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
