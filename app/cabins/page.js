bf39e3e5b8c6fc1a35045e8eaf547ec77369a817
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-04-17 08:51:59 +0300

    Add background image (bg.png) to public directory

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,18 +1,11 @@
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  if (!res.ok) {
-    throw new Error("Failed to fetch users");
-  }
-  const usersData = await res.json();
+export const metadata = {
+  title: "Cabins",
+};
 
+export default function Page() {
   return (
     <div>
       <h1>Cabins</h1>
-      <ul>
-        {usersData.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
     </div>
   );
 }
